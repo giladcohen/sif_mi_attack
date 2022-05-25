b5c76decc80b882330bc64a6a424ead3490bbea9
gilad <giladdalig@gmail.com>
2022-05-25 23:11:43 +0300

    u

--- init_project.sh
+++ init_project.sh
@@ -7,4 +7,4 @@ export PIP_REQUIRE_VIRTUALENV=true
 export PIP_RESPECT_VIRTUALENV=true
 virtualenv -p /usr/bin/python3 --clear --no-site-packages $SIF_PATH/.venv/sif_env
 source $SIF_PATH/.venv/sif_env/bin/activate
-pip install -r $SIF_PATH/requirements.txt --force-reinstall --extra-index-url https://download.pytorch.org/whl/cu11
+pip install -r $SIF_PATH/requirements.txt --force-reinstall --extra-index-url https://download.pytorch.org/whl/cu113
