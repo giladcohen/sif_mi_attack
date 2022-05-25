bf092351686e5c0be9799963fde8729efa84fa26
gilad <giladdalig@gmail.com>
2022-05-25 22:09:53 +0300

    add init project script

---
+++ init_project.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# assumption:
+# * virtualenv is avaliable and working
+
+export SIF_PATH=$(pwd)
+export PIP_REQUIRE_VIRTUALENV=true
+export PIP_RESPECT_VIRTUALENV=true
+virtualenv -p /usr/bin/python3 --clear --no-site-packages $SIF_PATH/.venv/sif_env
+source $SIF_PATH/.venv/sif_env/bin/activate
+pip install -r $SIF_PATH/requirements.txt --force-reinstall
